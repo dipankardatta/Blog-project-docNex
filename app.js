812dbdce4af6fac6498ccfe5330afd4330bd6ecf
Your Name <you@example.com>
2023-09-17 21:24:59 +0530

    created a connection between user and blog

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const sequelize = require("./util/database");
+const dotenv = require("dotenv").config();
+const User = require("./model/userModel");
+const BlogPost = require("./model/blogModel");
+
 const app = express();
 
 //Middleware
 app.use(bodyParser.json());
 
-app.listen(2000, () => {
-  console.log("Server has started using nodemon");
-});
+User.hasMany(BlogPost);
+BlogPost.belongsTo(User);
+
+sequelize
+  .sync({ force: true })
+  //   .sync()
+  .then((res) => {
+    app.listen(2000);
+  })
+  .catch((e) => console.log(e));
